04e12b432687e392407dd11ec9eb7f039e1f6e1c
DarkKezar <kezar.ru@gmail.com>
2021-10-31 16:37:42 +0300

    Добавьте файлы проекта.

---
+++ Infrastucture/Repositories/ForTest/HotelsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Repositories.ForTest
+{
+    class HotelsRepository
+    {
+        return new List<Hotel>(
+            new Hotel()
+        );
+    }
+}
